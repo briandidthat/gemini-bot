e20dcc581ce9bc6a6655c39e0a1bfd3709017284
briandidthat <brian@brians-mbp.mynetworksettings.com>
2024-04-29 15:13:54 -0400

    readme

--- logger.py
+++ logger.py
@@ -7,15 +7,15 @@ handler = logging.StreamHandler(stream=None)
 handler.setFormatter(
     jsonlogger.JsonFormatter(
         fmt="%(name)s %(asctime)s %(levelname)s %(message)s",
-        rename_fields={"asctime": "timestamp", "levelname": "level"},
+        rename_fields={"name": "logger", "asctime": "timestamp", "levelname": "level"},
     )
 )
 
-logger = logging.getLogger("MainLogger")
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+discord_logger = logging.getLogger("discord-logger")
+discord_logger.addHandler(handler)
+discord_logger.setLevel(logging.INFO)
 
 
-agent_logger = logging.getLogger("AgentLogger")
+agent_logger = logging.getLogger("agent-logger")
 agent_logger.addHandler(handler)
 agent_logger.setLevel(logging.INFO)
