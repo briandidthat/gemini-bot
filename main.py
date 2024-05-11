fc71b143950c8bc01bd22468e5582ef211204a3f
briandidthat <brian@brians-mbp.mynetworksettings.com>
2024-05-10 21:54:48 -0400

    refactor

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import discord
 import google.generativeai as genai
 
 from agent import ChatAgent, VisionAgent
-from bot import Bot, BotCog, ChatAgentCog
+from bot import Bot, BotCog
 
 
 # grab api keys from environment
@@ -21,8 +21,6 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 # initialize chat agent instance for content generation
 chat_agent = ChatAgent(model_name="gemini-1.0-pro-latest")
-# initialize AgentCog instance for scheduling tasks related to the chat agent
-chat_agent_cog = ChatAgentCog(chat_agent, timedelta(days=CHAT_TTL))
 
 # initialize vision agent instance for image content generation
 vision_agent = VisionAgent(model_name="gemini-pro-vision")
@@ -33,18 +31,17 @@ intents.message_content = True
 
 # create bot instance
 bot = Bot(chat_agent, vision_agent, DAILY_LIMIT, command_prefix="$", intents=intents)
-# initialize BogCog instance for bot commands
-bot_cog = BotCog(bot, BOT_OWNER)
+# initialize BogCog instance for bot commands and scheduled tasks
+bot_cog = BotCog(bot, BOT_OWNER, CHAT_TTL)
 
 
-# register the cogs with the bot
+# register the cog with the bot
 async def register():
     await bot.add_cog(bot_cog)
-    await bot.add_cog(chat_agent_cog)
 
 
 if __name__ == "__main__":
-    # run the coroutine to register the cogs
+    # run the coroutine to register the cog
     asyncio.run(register())
     # run the bot
     bot.run(token=DISCORD_TOKEN)
